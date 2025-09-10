4a0409de77c194f86bde9ba347389ff72a493c05
niyati-dhanani <niyatidhanani05@gmail.com>
2025-09-10 11:34:53 +0530

    project-1

--- project-1/server.js
+++ project-1/server.js
@@ -1,41 +1,33 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
-const server = http.createServer((req, res) => {
-  let filepath = '';
-
-  switch (req.url) {
-    case '/':
-      filepath = './index.html';
-      break;
-    case '/blog':
-      filepath = './blog.html';
-      break;
-    case '/services':
-      filepath = './services.html';
-      break;
-    case '/contact':
-      filepath = './contact.html';
-      break;
-    case '/gallery':
-      filepath = './gallery.html';
-      break;
-    default:
-      filepath = './index.html'; // fallback
-  }
-
-  try {
-    const data = fs.readFileSync(filepath, 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+const server = http.createServer((req,res) =>{
+    let filepath ="";
+    switch (req.url){
+        case "/":
+            filepath="./index.html"
+            break;
+        case "/about":
+            filepath="./about.html"
+            break;
+        case "/menu":
+            filepath="./menu.html"
+            break;
+        case "/gallery":
+            filepath="./gallery.html"
+            break;
+        case "/contact":
+            filepath="./contact.html"
+            break;
+        default:
+            filepath="./notfound.html"
+            break;
+    }
+    let data = fs.readFileSync(filepath,'utf-8');
     res.end(data);
-  } catch (err) {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>404 Page Not Found</h1>');
-  }
 });
 
 const port = 8000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+server.listen(port,() => {
+    console.log(`server start at http://localhost:${port}`);
 });
